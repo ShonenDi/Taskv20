c9ebaea3972ec7784c8be728372cadc1056196d3
Dima Usatyuk <shonen1988@gmail.com>
2019-10-24 11:41:51 +0300

    Great two txt files with abstract path

--- src/FileCompare.java
+++ src/FileCompare.java
@@ -1,9 +1,20 @@
 import java.io.File;
+import java.io.IOException;
 
 public class FileCompare {
     public static void main(String[] args) {
         File fileOne = new File("test1.txt");
         File fileTwo = new File("test2.txt");
+        try {
+            fileOne.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }try {
+            fileTwo.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 
 }
