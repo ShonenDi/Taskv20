1f390e22c69538235ad858c9c03148edd52e6916
Dima Usatyuk <shonen1988@gmail.com>
2019-10-24 11:25:01 +0300

    Great two txt Files

--- src/FileCompare.java
+++ src/FileCompare.java
@@ -1,2 +1,9 @@
+import java.io.File;
+
 public class FileCompare {
+    public static void main(String[] args) {
+        File fileOne = new File("test1.txt");
+        File fileTwo = new File("test2.txt");
+    }
+
 }
