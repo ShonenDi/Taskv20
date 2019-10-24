5b0e2b27aeb4e827c28d4473c9dc51cfd32da2bf
Dima Usatyuk <shonen1988@gmail.com>
2019-10-24 16:23:55 +0300

    File convert to ArrayList<String>;

--- src/FileCompare.java
+++ src/FileCompare.java
@@ -1,21 +1,31 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class FileCompare {
     public static void main(String[] args) {
         File f1 = new File("test1.txt");
-       // System.out.println(txtFileToWords(f1).toString());
-        String[] list = f1.list();
+        System.out.println(txtFileToWords(f1).toString());
+
 
     }
 
     public static ArrayList<String> txtFileToWords(File file) {
-        String[] words = file.list();
-        ArrayList<String> myWord = new ArrayList<String>();
-        for (String word : words) {
-            myWord.add(String.valueOf(word.split("[ .,\n!]")));
+        String word ="";
+        List<String> words = new ArrayList<>();
+        try {
+            Scanner sc = new Scanner(file).useDelimiter(",\\s*");
+            while (sc.hasNext()){
+                word =sc.next();
+                words.add(word);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-        return myWord;
+        return (ArrayList<String>) words;
     }
 }
 
