d3f6d04965b2679404904dcc1e3e342e681c2052
Dima Usatyuk <shonen1988@gmail.com>
2019-10-28 11:54:29 +0200

    Add coments

--- src/FileCompare.java
+++ src/FileCompare.java
@@ -4,26 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/*
+Напишите программу, которая примет на вход два
+текстовых файла, а вернет один. Содержимым этого файла
+должны быть слова, которые одновременно есть и в первом и
+во втором файле.
+ */
+
 public class FileCompare {
     public static void main(String[] args) {
         File f1 = new File("test1.txt");
         System.out.println(txtFileToWords(f1).toString());
         txtFileToWords(f1);
         int size = txtFileToWords(f1).size();
-        for(String word:txtFileToWords(f1)){
+        for (String word : txtFileToWords(f1)) {
             System.out.println(word);
         }
 
 
     }
 
+    /**
+     * method <code>txtFileToWords</code> convert txt file to <code>ArrayList</code>
+     * String <code>word</code> param to add all word in text file to <code>ArrayList</code>
+     * @param file
+     * @return
+     */
     public static ArrayList<String> txtFileToWords(File file) {
-        String word ="";
+        String word = "";
         List<String> words = new ArrayList<>();
         try {
             Scanner sc = new Scanner(file).useDelimiter("\\s");
-            while (sc.hasNext()){
-                word =sc.next();
+            while (sc.hasNext()) {
+                word = sc.next();
                 words.add(word);
             }
 
